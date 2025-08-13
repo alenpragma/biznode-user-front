70b0fef66e05f8176332d6312f06548bf144149e
remontripura <remontripura045@gmail.com>
2025-08-13 15:17:39 +0600

    udpate

--- src/types/loginType/loginType.ts
+++ src/types/loginType/loginType.ts
@@ -1,12 +1,23 @@
-
 export type LoginResponse = {
   data: {
     data?: {
       token: string;
-    }
+    };
     errors?: {
-      email: string
-    }
+      email: string;
+    };
+    success: boolean;
+    message?: string | undefined;
+  };
+  status: number;
+  success: boolean;
+};
+export type signUpResponse = {
+  data?: {
+    token: string;
+    errors?: {
+      email: string;
+    };
     success: boolean;
     message?: string | undefined;
   };
